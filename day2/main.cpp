654f2f5b6352368ebb1fd3f60c8174d2875fae49
Ionuț Leonte <ionut.leonte@gmail.com>
2021-12-02 18:33:16 +0200

    day2: complete

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,1 +1,33 @@
-int main() { return 0; }
+#include <aoc.h>
+
+int main() {
+    std::string line{};
+    int64_t x{}, y{}, aim{}, depth{};
+    int64_t delta{};
+
+    while (std::getline(std::cin, line)) {
+        auto sv = aoc::trim(aoc::to_sv(line));
+
+        if (aoc::substr(sv, 0, 7) == "forward") {
+            aoc::from_chars(aoc::substr(sv, 8), delta);
+            x += delta;
+            depth += aim * delta;
+        } else if (aoc::substr(sv, 0, 4) == "down") {
+            aoc::from_chars(aoc::substr(sv, 5), delta);
+            y += delta;
+            aim += delta;
+        } else if (aoc::substr(sv, 0, 2) == "up") {
+            aoc::from_chars(aoc::substr(sv, 3), delta);
+            y -= delta;
+            aim -= delta;
+        }
+    }
+
+    auto part1 = x * y;
+    auto part2 = x * depth;
+
+    fmt::print("{}\n", part1);
+    fmt::print("{}\n", part2);
+
+    return 0;
+}
